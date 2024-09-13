d6f2c5536ec156b4cd21da3bb41a5465d083f85a
Марион Бахшиева <landroverx.wow@gmail.com>
2024-09-13 19:47:03 +0300

    frontend

--- src/components/Post.js
+++ src/components/Post.js
@@ -6,17 +6,15 @@ import TimeAgo from './TimeAgo';
 export default function Post({ post }) {
   return (
     <Stack direction="horizontal" gap={3} className="Post">
-      <Image src={post.author.avatar_url + '&s=48'}
-             alt={post.author.username} roundedCircle />
       <div>
         <p>
-          <Link to={'/user/' + post.author.username}>
-            {post.author.username}
+          <Link to={'/user/' + post.name}>
+            {post.login}
           </Link>
           &nbsp;&mdash;&nbsp;
-          <TimeAgo isoDate={post.timestamp} />:
+          <TimeAgo isoDate={post.email} />:
         </p>
-        <p>{post.text}</p>
+        <p>{post.telegram_id}</p>
       </div>
     </Stack>
   );
